32b66d5087a80e72f89355aa2a21ec0a0af6b4e0
Adelheid483 <adelheid0106@gmail.com>
2021-11-21 14:07:17 +0300

    2-a_layout: finished with templates

--- ViewModels/CarsListViewModel.cs
+++ ViewModels/CarsListViewModel.cs
@@ -3,7 +3,7 @@ using asp_shop.Data.Models;
 
 namespace asp_shop.ViewModels
 {
-    public class CarsListViewModel
+    public class CarsListViewModel // Model for template
     {
         public IEnumerable<CarModel> GetAllCars { get; set; }
         public string CurrentCategory { get; set; }
